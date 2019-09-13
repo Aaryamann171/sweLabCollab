57c40001acffb6e99caaedb27cce298eed262b1f
Aaryamann Sharma <aaryamann171@gmail.com>
2019-09-13 17:13:38 +0530

    added integration code to main file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("this is main file")
+import addition
+import division
+import subtraction
+import multiplication
+
+print("We are taking numbers 10 and 5")
+print("This is addition: ")
+addition.add(10,5)
+print("This is subtraction: ")
+subtraction.sub(10,5)
+print("This is multiplication: ")
+multiplication.multiplication(10,5)
+print("This is division: ")
+division.division(10,5)
